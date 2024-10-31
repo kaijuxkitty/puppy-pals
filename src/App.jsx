685f10471255057ead798854c002c63dbd146e82
kaijuxkitty <kaijuxkitty@gmail.com>
2024-10-31 11:27:36 -0500

    puppy names not clickable

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {puppyList} from './data.js'
 import {useState} from 'react' 
 function App() {
 const [puppies, setPuppies] = useState(puppyList)
+const [featPupId, setFeatPupId] = useState(null)
 return (
     <div>
   {puppies.map((puppy) => {
